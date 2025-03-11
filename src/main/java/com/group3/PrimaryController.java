70446ef9473a699f1e2b807908a9e12769267114
P-William <w.paetz@uzwelo.com>
2025-03-11 09:13:10 -0400

    Project Setup

---
+++ src/main/java/com/group3/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.group3;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class PrimaryController {
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
