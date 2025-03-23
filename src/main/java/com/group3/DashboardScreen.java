7b8388bec08c83c960378e680869e79a0e0f77f3
Brooke Cronin <60623477+brookec123@users.noreply.github.com>
2025-03-22 22:12:21 -0400

    began gui for dashboard

---
+++ src/main/java/com/group3/DashboardScreen.java
@@ -0,0 +1,34 @@
+package com.group3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class DashboardScreen extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Load FXML layout
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/group3/dashboardLayout.fxml"));
+        BorderPane root = loader.load();
+
+        // Set up the scene
+        Scene scene = new Scene(root, 1440, 1024);
+
+        // Add CSS
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/group3/dashboardStyle.css")).toExternalForm());
+//        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/accord/aCCORD-logo.png"))));
+        // Configure the stage
+        primaryStage.setTitle("Doctor Tracker");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
